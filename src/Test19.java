eb26016f5077e0d553c42c2db1ee5f39c682ae0d
Sasza <rudojsru@gmail.com>
2019-02-09 17:38:10 +0100

    commit 19 last

--- src/Test19.java
+++ src/Test19.java
@@ -1,3 +1,24 @@
 public class Test19 {
+    public static void main(String[] args) {
+        int x []={1,4,6,11,88};
+        System.out.println(centeredAdverage(x));
+    }
 
+    public static int centeredAdverage (int[] nums){
+        if(nums == null || nums.length <3){
+            throw new IllegalArgumentException();
+        }
+        int min =nums[0];
+        int max =nums[0];
+        int sum =0;
+        for (int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(nums[i] > max){
+                max = nums[i];
+            }else if (nums[i] <min ){
+                min =nums [i];
+            }
+        }
+      return (sum-(max+min))/(nums.length-2);
+    }
 }
